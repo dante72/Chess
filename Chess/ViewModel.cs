ee3c88dc1338e9d06a464486e1c6e5a48337bd5e
dante72 <turbosuns7@gmail.com>
2021-11-18 18:04:37 +0300

    listBox style (creating board)

---
+++ Chess/ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    public class ViewModel
+    {
+        public List<int> СhessBoard { set; get; }
+
+        public ViewModel()
+        {
+            СhessBoard = new List<int>();
+            for (int i = 0; i < 64; i++)
+                СhessBoard.Add(i + 1);
+        }
+    }
+}
